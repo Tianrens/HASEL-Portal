46cea2fc74628b3efdf9f6b377507d921d67790a
Milk <50122790+Milk-Yan@users.noreply.github.com>
2021-08-25 00:22:26 +1200

    Enhancement/44/Setup email (#2)
    * add utility for send emails

---
+++ backend/src/email/sendEmail.js
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer';
+
+export function sendEmail(recipientEmail, emailSubject, emailText) {
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_ADDRESS,
+            pass: process.env.EMAIL_PASSWORD
+        },
+    });
+
+    const mailOptions = {
+        to: recipientEmail,
+        subject: emailSubject,
+        text: emailText
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            throw error;
+        } else {
+            console.log(`Email sent: ${  info.response}`);
+        }
+    });
+}
