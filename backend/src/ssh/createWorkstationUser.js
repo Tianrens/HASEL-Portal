08a36815d4c90252bda9b2b0591e7dc2bde5de8d
Tianrens <tshe695@aucklanduni.ac.nz>
2021-09-01 14:03:53 +1200

    Enchancement/114/ssh sudo scripts (#22)
    * added support for sudo commands
    
    * comments for execCommand
    
    * updated comments for createworkstationuser
    
    * extracted sudo options
    
    * added comment about status code

--- backend/src/ssh/createWorkstationUser.js
+++ backend/src/ssh/createWorkstationUser.js
@@ -1,10 +1,10 @@
 import { execCommand } from '.';
 
 /**
- * @param {*} host 
- * @param {*} upi 
+ * @param {string} host ip address of machine to ssh into
+ * @param {string} upi the account to be created
  * @param {*} daysInactive days after expire date till account is permanently disabled.
- * @param {*} expireDate YYYY-MM-DD
+ * @param {string} expireDate YYYY-MM-DD
  */
 export async function createWorkstationUser(host, upi, daysInactive, expireDate) {
     const addUserResult = await execCommand(host, `sudo useradd -m ${upi} -f ${daysInactive} -e ${expireDate}`);
@@ -13,7 +13,8 @@ export async function createWorkstationUser(host, upi, daysInactive, expireDate)
 
     const addUserGroupResult = await execCommand(host, `sudo usermod -aG hasel-users ${upi}`);
 
-    if (addUserResult.stderr || changePasswordResult.stderr || addUserGroupResult.stderr) {
+    // status code is null if process was successful
+    if (addUserResult.code || changePasswordResult.code || addUserGroupResult.code) {
         const errorMessage = 'Error with creating workstation user account.';
         throw errorMessage;
     }
--- backend/src/ssh/execCommand.js
+++ backend/src/ssh/execCommand.js
@@ -1,15 +1,21 @@
 const { NodeSSH } = require('node-ssh');
 
+/**
+ * @param {string} host ip address of machine to ssh to
+ * @param {string} command can include sudo commands
+ * @returns
+ */
 export async function execCommand(host, command) {
-    const ssh = new NodeSSH();
+    const hasSudo = command.includes('sudo');
 
+    const ssh = new NodeSSH();
     await ssh.connect({
         host,
         username: process.env.SSH_USERNAME,
         password: process.env.SSH_PASSWORD,
     });
-    const result = await ssh.execCommand(command);
-
+    const sudoOptions = hasSudo ? { execOptions: { pty: true }, stdin: `${process.env.SSH_PASSWORD}\n` } : {};
+    const result = await ssh.execCommand(command, sudoOptions);
     ssh.dispose();
 
     return result;
